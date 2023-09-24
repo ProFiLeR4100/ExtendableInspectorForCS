2ab8d0abf87e5f9b08d3dd716aaef1616e9da7ab
Mikhail <profiler4100v2@gmail.com>
2023-09-24 21:38:21 +0300

    Fixed NullPointerException
    Godot doesn't provide GodotObject for unknown reason, so I added null-safe operator to checks

--- addons/extendable_inspector_for_cs/core/ExtendableInspector.cs
+++ addons/extendable_inspector_for_cs/core/ExtendableInspector.cs
@@ -25,25 +25,25 @@ public partial class ExtendableInspector : EditorInspectorPlugin {
 	}
 
 	public override void _ParseBegin(GodotObject godotObject) {
-		if (godotObject.HasMethod("ExtendInspectorBegin")) {
+		if (godotObject?.HasMethod("ExtendInspectorBegin") ?? false) {
 			godotObject.Call("ExtendInspectorBegin", this);
 		}
 	}
 
 	public override void _ParseEnd(GodotObject godotObject) {
-		if (godotObject.HasMethod("ExtendInspectorEnd")) {
+		if (godotObject?.HasMethod("ExtendInspectorEnd") ?? false) {
 			godotObject.Call("ExtendInspectorEnd", this);
 		}
 	}
 
 	public override void _ParseCategory(GodotObject godotObject, string category) {
-		if (godotObject.HasMethod("ExtendInspectorCategory")) {
+		if (godotObject?.HasMethod("ExtendInspectorCategory") ?? false) {
 			godotObject.Call("ExtendInspectorCategory", this, category);
 		}
 	}
 
 	public override  bool _ParseProperty(GodotObject godotObject, Variant.Type type, string name, PropertyHint hintType, string hintString, PropertyUsageFlags usageFlags, bool wide) {
-		if (godotObject.HasMethod("ExtendInspectorProperty")) {
+		if (godotObject?.HasMethod("ExtendInspectorProperty") ?? false) {
 			return godotObject.Call("ExtendInspectorProperty", this, (long) type, name, (long) hintType, hintString, (long) usageFlags, wide).AsBool();
 		}
 
@@ -51,7 +51,7 @@ public partial class ExtendableInspector : EditorInspectorPlugin {
 	}
 
 	public override void _ParseGroup(GodotObject godotObject, string group) {
-		if (godotObject.HasMethod("ExtendInspectorGroup")) {
+		if (godotObject?.HasMethod("ExtendInspectorGroup") ?? false) {
 			godotObject.Call("ExtendInspectorGroup", this, group);
 		}
 	}
